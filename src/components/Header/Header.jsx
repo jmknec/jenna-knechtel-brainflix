98b31ebc68a1d5b78f34231c43377a487fa3fbb6
Jenna Knechtel <jmknec@gmail.com>
2024-05-28 22:44:24 -0400

    add responsive styles header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,31 +13,39 @@ function Header(props) {
         alt="BrainFlix logo white play button blue background"
       ></img>
       <div className="main-nav">
-        <search className="main-nav__search">
-          <form className="main-nav__form" action="./search">
-            {/* <i className="main-nav__search-icon"></i> */}
-            <input
-              className="main-nav__input"
-              type="search"
-              name="search-field"
-              id="search"
-              placeholder="Search"
-            />
-          </form>
-        </search>
-        <Avatar
-          className="main-nav__user"
-          image={Image}
-          alt="User profile picture"
-        />
+        <div className="main-nav__site">
+          <search className="main-nav__search">
+            <form className="main-nav__form" action="./search">
+              <input
+                className="main-nav__input"
+                type="search"
+                name="search-field"
+                id="search"
+                placeholder="Search"
+              />
+            </form>
+          </search>
+          <Avatar
+            className="main-nav__avatar main-nav__avatar--mobile"
+            image={Image}
+            alt="User profile picture"
+          />
+        </div>
+        <div className="main-nav__user">
+          <Button
+            className="main-nav__upload"
+            btnLabel="Upload"
+            icon={Upload}
+            iconClass="main-nav__upload-icon"
+            altText="up arrow above horizontal line"
+          />
+          <Avatar
+            className="main-nav__avatar main-nav__avatar--tablet"
+            image={Image}
+            alt="User profile picture"
+          />
+        </div>
       </div>
-      <Button
-        className="main-nav__upload"
-        btnLabel="Upload"
-        icon={Upload}
-        iconClass="main-nav__upload-icon"
-        altText="up arrow above horizontal line"
-      />
     </header>
   );
 }
