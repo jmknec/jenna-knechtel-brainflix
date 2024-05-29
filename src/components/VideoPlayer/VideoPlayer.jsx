6725ea782e22854643ce9e90161857d7eb5fa256
Jenna Knechtel <jmknec@gmail.com>
2024-05-29 14:35:23 -0400

    create video player component (static)

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,15 @@
-import VideoList from "../VideoList/VideoList";
+import "./VideoPlayer.scss";
 
 function VideoPlayer() {
   return (
     <div className="active-video">
-      <video className="active-video__player" src=""></video>
+      <video
+        className="active-video__player"
+        controls
+        poster="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+      >
+        <source src="https://unit-3-project-api-0a5620414506.herokuapp.com/stream" />
+      </video>
     </div>
   );
 }
