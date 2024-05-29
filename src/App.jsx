e670a0d4aa9184b5cc9250eb21eb5da332d72aa2
Jenna Knechtel <jmknec@gmail.com>
2024-05-29 18:39:50 -0400

    add tablet styles comment form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import VideoList from "./components/NextVideos/NextVideos";
 import MainSection from "./components/MainSection/MainSection";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 function App() {
   return (
     <>
       <Header />
+      <VideoPlayer />
       <MainSection />
       <VideoList />
     </>
--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -6,7 +6,6 @@ import CommentForm from "../CommentForm/CommentForm";
 function MainSection() {
   return (
     <main className="main">
-      <VideoPlayer />
       <VideoDetails
         title="The Future of Artificial Intelligence"
         description="Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate interplay between innovation, ethics, and the ever-evolving tech frontier."
