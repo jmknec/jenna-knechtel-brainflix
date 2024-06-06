d0a893231142a842aa7db4972c79348206a83a38
Jenna Knechtel <jmknec@gmail.com>
2024-06-06 12:18:09 -0400

    add functions - fetch videos list & video details

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import UploadPage from "./pages/UploadPage/UploadPage";
 
 function App() {
-  const baseUrl = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
-  const apiKey = "6ab3a096-a9af-4480-80df-8f33eec68532";
-
-  const [videosList, setVideosList] = useState(null);
-
-  useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        const videosResponse = await axios.get(
-          `${baseUrl}videos?api_key=${apiKey}`
-        );
-        setVideosList(videosResponse.data);
-        console.log(videosList);
-        console.log(videosList[0]);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchVideos();
-  }, []);
-
   return (
     <BrowserRouter>
       <div className="app-container">
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<HomePage />} />
           <Route path="upload" element={<UploadPage />} />
         </Routes>
       </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,69 @@
-import { useState } from "react";
-import videos from "../../data/video-details.json";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+// import videos from "../../data/video-details.json";
 import "./HomePage.scss";
-import ActiveVideoPlayer from "../../components/ActiveVideoPlayer/ActiveVideoPlayer";
-import MainSection from "../../components/MainSection/MainSection";
-import VideoList from "../../components/VideoList/VideoList";
+// import ActiveVideoPlayer from "../../components/ActiveVideoPlayer/ActiveVideoPlayer";
+// import MainSection from "../../components/MainSection/MainSection";
+// import VideoList from "../../components/VideoList/VideoList";
 
 function HomePage() {
-  const [selectedVideo, setSelectedVideo] = useState(
-    "84e96018-4022-434e-80bf-000ce4cd12b8"
-  );
-  const selectedVideoObj = videos.find((video) => {
-    return video.id === selectedVideo;
-  });
+  const baseUrl = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
+  const apiKey = "6ab3a096-a9af-4480-80df-8f33eec68532";
+
+  const [videosList, setVideosList] = useState([]);
+  const [videoDetails, setVideoDetails] = useState(null);
+  const { id } = useParams();
+
+  // useEffect(() => {
+  const fetchVideos = async () => {
+    try {
+      const videosResponse = await axios.get(
+        `${baseUrl}videos?api_key=${apiKey}`
+      );
+      setVideosList(videosResponse.data);
+      console.log(videosList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // fetchVideos();
+  // }, []);
+
+  // useEffect(() => {
+  const fetchVideoDetails = async () => {
+    try {
+      const detailsResponse = await axios.get(
+        `${baseUrl}videos/${id}?api_key=${apiKey}`
+      );
+      setVideoDetails(detailsResponse.data);
+      console.log(videoDetails);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // fetchVideoDetails();
+  // }, []);
+
+  useEffect(() => {
+    if (id) {
+      fetchVideoDetails();
+      return;
+    }
+    fetchVideos();
+  }, []);
+
+  // const [selectedVideo, setSelectedVideo] = useState(
+  //   "84e96018-4022-434e-80bf-000ce4cd12b8"
+  // );
+  // const selectedVideoObj = videos.find((video) => {
+  //   return video.id === selectedVideo;
+  // });
 
   return (
     <>
-      <ActiveVideoPlayer selectedVideoObj={selectedVideoObj} />
-      <div className="main-container">
+      {/* <ActiveVideoPlayer selectedVideoObj={selectedVideoObj} /> */}
+      {/* <div className="main-container">
         <MainSection
           selectedVideo={selectedVideo}
           selectedVideoObj={selectedVideoObj}
@@ -25,7 +72,7 @@ function HomePage() {
           selectedVideo={selectedVideo}
           setSelectedVideo={setSelectedVideo}
         />
-      </div>
+      </div> */}
     </>
   );
 }
