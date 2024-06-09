3f6a47cbee0e1b677715a6e5c8947ef224a35418
Jenna Knechtel <jmknec@gmail.com>
2024-06-08 22:46:13 -0400

    add default upload image

--- src/components/UploadImage/UploadImage.jsx
+++ src/components/UploadImage/UploadImage.jsx
@@ -1,11 +1,15 @@
 import "./UploadImage.scss";
 
-function UploadImage({ image }) {
+function UploadImage() {
   return (
     <>
       <div className="upload__image-container">
         <h2 className="upload__image-heading">Video Thumbnail</h2>
-        <img className="upload__image" src={image} alt="" />
+        <img
+          className="upload__image"
+          src="src/assets/images/Upload-video-preview.jpg"
+          alt="Lower body of relay runner in starting position"
+        />
       </div>
     </>
   );
