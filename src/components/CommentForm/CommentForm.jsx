09a31e64abaf097c2a9ca45ee76d7f27cdeedb82
Jenna Knechtel <jmknec@gmail.com>
2024-06-16 11:54:50 -0400

    changes and then reverts changes

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,38 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
 import "./CommentForm.scss";
 import Avatar from "../Avatar/Avatar";
 import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
 
 function CommentForm() {
-  const baseUrl = import.meta.env.VITE_API_URL;
-  const [comment, setComment] = useState("");
-  const { id } = useParams();
-
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const sendCommentObj = async (commentObj) => {
-    try {
-      const commentRes = await axios.post(`${baseUrl}/videos/${id}/comments`);
-      return commentRes;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const commentData = { comment };
-
-    sendCommentObj(commentData);
-  };
-
   return (
-    <form className="comment-form" onSubmit={submitHandler}>
+    <form className="comment-form">
       <div className="comment-form__avatar-container">
         <Avatar
           className="comment-form__avatar"
@@ -58,7 +31,6 @@ function CommentForm() {
           icon="src/assets/icons/add_comment.svg"
           iconClass="comment-form__icon"
           altText="Blue addition sign on white text bubble"
-          type="submit"
         />
       </div>
     </form>
